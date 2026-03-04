c3f67ee9ed95adcf9eba5befac6c16a06097f120
Danilo Andres Doria Diaz <danilodoria519@gmail.com>
2026-03-04 18:28:45 -0500

    Ejercicio de cajero finalizado

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SimuladorDeCajeroAutomaticoBasico
 Proyecto básico para en consola que simule operaciones básicas de un cajero automático.
+
+## Prerrequisitos
+- Instalar python3 en el sistema operativo.
+- Instalar Visual Studio Code.
+- Instalar las extenciones oficiales de Python.
+- Tener conocimientos de ciclos y condicionales.
+- Tener conocimiento en manejo de errores (Try - Except),
+
+## Autor
+Danilo Andres Doria Diaz
--- main.py
+++ main.py
@@ -1,34 +1,60 @@
 balance = 1000
-quantity_op = int(input("Cuantas operaciones desea realizar? "))
+
+while True:
+    try:
+        quantity_op = int(input("Cuantas operaciones desea realizar? "))
+        if quantity_op < 0:
+            print("\nPor favor ingrese un valor valido \n")
+            continue
+        break
+    except ValueError:
+        print("\nPor favor solo digite valores numericos \n")
 
 for i in range(quantity_op):
-    op = int(input("1 → Consultar saldo \n2 → Retirar Dinero \n3 → Depositar Dinero \n"))
 
+    while True:
+        try:
+            op = int(input("\n1 → Consultar saldo \n2 → Retirar Dinero \n3 → Depositar Dinero \n"))
+
+            if op not in (1, 2, 3):
+                print("\nOpción inválida \n")
+                continue
+            break
+        except ValueError:
+            print("\nPor favor solo digite valores numericos \n")
+    
     match op:
         case 1:
             print(balance)
         case 2:
             while True:
-                mount = float(input("Ingrese el monto a retirar "))
-                if mount < 0:
-                    print("Ingrese un monto valido ")
-                    continue
-                else:
-                    if mount >= balance:
-                        print("Fondos insuficientes")
+                try:
+                    mount = float(input("Ingrese el monto a retirar "))
+                    if mount < 0:
+                        print("Ingrese un monto valido \n")
+                        continue
                     else:
-                        balance -= mount
-                        print(f"\nEl nuevo saldo es: {balance}\n")
-                break
+                        if mount > balance:
+                            print("Fondos insuficientes")
+                        else:
+                            balance -= mount
+                            print(f"\nEl nuevo saldo es: {balance}\n")
+                    break
+                except ValueError:
+                    print("Por favor solo digite valores numericos \n")
         case 3:
             while True:
-                mount = float(input("Ingrese el monto a depositar "))
-                if mount < 0:
-                    print("Ingrese un monto valido ")
-                    continue
-                else:
-                    balance += mount
-                    print(f"\nEl nuevo saldo es: {balance}\n")
-                break
+                try:
+                    mount = float(input("Ingrese el monto a depositar "))
+                    if mount < 0:
+                        print("Ingrese un monto valido \n")
+                        continue
+                    else:
+                        balance += mount
+                        print(f"\nEl nuevo saldo es: {balance}\n")
+                    break
+                except ValueError:
+                    print("Por favor solo digite valores numericos \n")
         case _:
             print("Error")
+print("\nGracias por usar el cajero automático")
